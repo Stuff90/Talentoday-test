a9bf3edafc877473c65b9a1d4e4347c4197619cf
Stuff90 <simon@pc21.home>
2014-08-17 21:45:02 +0200

    master * Add all css & less management

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/*jshint node:true*/
+
+module.exports = function (grunt) {
+
+    'use strict';
+
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        watch: {
+            less: {
+                files: ['style/less/**'],
+                tasks: ['less:dev'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            }
+        },
+
+
+        less: {
+            dev: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'style/main.css': 'style/Less/main.less'
+                }
+            },
+            prod: {
+                options: {
+                    compress: true,
+                    report: true,
+                    sourceMap: false
+                },
+                files: {
+                    'style/main.css': 'style/Less/main.less'
+                }
+            }
+        }
+
+    });
+
+    // Tasks.
+    grunt.registerTask('default', ['jshint', 'build']);
+
+    grunt.registerTask('prod', [
+        'less:prod'
+    ]);
+
+};
