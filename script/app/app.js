db655f9798bc81c4ff02ce3a69a2d87d9c23c9ad
Stuff90 <bernard.simon.90@gmail.com>
2014-08-18 20:48:32 +0200

    master * Clean app main file

--- script/app/app.js
+++ script/app/app.js
@@ -8,18 +8,19 @@ contributors :
 define([
 	'angular',
 	'angularRoute',
+
 	'modules/home/home.controller',
+	// 'directives/editInPlace',
+
 	'services/athlete.service',
+
 	], function (angular) {
 
 		// Declare app level module which depends on filters, and services
-		
+
 		return angular.module('app', [
 			'ngRoute',
-			// 'app.filters',
-			// 'app.services',
-			// 'app.directives',
 			'app.home',
-			// 'app.controllers'
+			// 'app.editInPlace',
 		]);
 });
